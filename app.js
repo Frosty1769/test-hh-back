be6119018af85c319014e548201c29bc70dabf5e
Frosty1769 <crazyremed@yandex.ru>
2025-06-18 11:49:48 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,46 @@
+import express from 'express'
+import cors from 'cors'
+
+import { readdirSync } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath, pathToFileURL } from 'url';
+
+export const app = express()
+const port = 3000
+
+//Базовые настройки
+app.use(express.json())
+app.use(cors({
+    credentials: true,
+    origin: "http://localhost:5173"
+}))
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+//Автоматическое подключение рутов
+readdirSync(join(__dirname, 'src/routes')).forEach(file => {
+    if (!file.endsWith('.js')) return; 
+    const routeName = file.replace('.js', '').toLowerCase();
+    const routePath = join(__dirname, 'src/routes', file);
+    const fileUrl = pathToFileURL(routePath).href;
+    
+    const module = import(fileUrl)
+        .then(module => {
+            app.use(`/api/${routeName}`, module.default || module);
+        })
+        .catch(err => {
+            console.error(`Ошибка загрузки роута ${file}:`, err);
+        });
+});
+
+//Лог ошибки
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ src/controllers/item.js
@@ -0,0 +1,41 @@
+import { Item } from "../schemas/item.js"
+import { array_move } from "../utils/array.js"
+
+//Базовые данные
+var dataList = [new Item({ id: 0, text: 0, isSelected: false })]
+
+for (let index = 1; index < 1000000; index++) {
+    dataList.push(new Item({ id: Number(index), text: Number(index), isSelected: false }));
+}
+
+//Контроллер для элементов
+class ItemController {
+    selectItem(id, isSelected, res) {
+        let index = dataList.findIndex(it => it.id == id)
+        if (index === -1) {
+            res.status(500).json({ status: "error", message: `get id: ${id} with ${isSelected}` })
+            return
+        }
+        dataList[index].isSelected = isSelected
+        res.status(200).json({ status: "success", message: `get id: ${id} with ${isSelected}` })
+    }
+
+    moveList(prevIndex, newIndex, res) {
+        let newArray = array_move(dataList, prevIndex, newIndex)
+        dataList = [...newArray];
+
+        res.status(200).json({ status: "success" })
+    }
+
+    getAllList(page, res, search) {
+        let offset = 20 * (page - 1)
+        res.status(200).json({
+            "status": 'success',
+            "data": dataList
+                        .filter(it => search ? it.text.includes(search) : true)
+                        .filter((it, ind) => ind >= offset && ind < 20 * page),
+        })
+    }
+}
+
+export default new ItemController()
